e4ecca661633bd04207b5270ef442fa8f61e29a7
Aditria Pardana <aditria.pardana@yahoo.co.id>
2021-12-17 21:24:05 +0700

    Commit add Slicing Component Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import 'assets/scss/style.scss';
+import LandingPage from 'pages/LandingPage';
 
 function App() {
-  return (
-    <div className="App">
-      Hello WorldJS!
+  return <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
-  );
 }
 
 export default App;
