1932bd1f2d46d66ae16692c0663117510256f8ab
Aditria Pardana <aditria.pardana@yahoo.co.id>
2021-12-21 17:13:15 +0700

    Add styling component Hero

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -77,6 +77,7 @@ Button.propTypes = {
     style: propTypes.string,
     className: propTypes.string,
     isExternal: propTypes.bool,
+    isPrimary: propTypes.bool,
     isDisabled: propTypes.bool,
     isLoading: propTypes.bool,
     isSmall: propTypes.bool,
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 
 import Header from 'parts/Header'
+import Hero from 'parts/Hero'
+
+import landingPage from "json/landingPage.json"
 
 export default class LandingPage extends Component{
     render(){
+        console.log(this.props)
         return(
             <>
                 <Header {...this.props}></Header>
+                <Hero data={landingPage.hero} />
             </>
         );
     }
