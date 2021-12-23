a8744f437b9cabb9f5d6e975efdeb04ee7b7ada0
Aditria Pardana <aditria.pardana@yahoo.co.id>
2021-12-23 15:10:23 +0700

    Add section MostPicked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,17 +1,27 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
 import landingPage from "json/landingPage.json";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
   render() {
     console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }
