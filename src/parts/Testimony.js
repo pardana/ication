dbaf55d5115d699f8e1490e83bdc9acbb4a06a9c
Aditria Pardana <aditria.pardana@yahoo.co.id>
2021-12-27 17:17:53 +0700

    update component testimonial

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,10 +1,53 @@
 import React from "react";
-import Star from "elements/Star";
+import TestimonialAccent from "assets/images/testimonial-landingpages-frame.jpg";
 
-export default function Testimony() {
+import Star from "elements/Star";
+import Button from "elements/Button";
+
+export default function Testimony({ data }) {
   return (
-    <div>
-      <Star value={4.5} width={35} height={35} spacing={4}></Star>
-    </div>
+    <section className="container">
+      <div className="row align-items-center">
+        <div className="col-auto" style={{ marginRight: 70 }}>
+          <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
+            <img
+              src={data.imageUrl}
+              alt="Testimonial"
+              className="position-absolute"
+              style={{ zIndex: 2 }}
+            />
+            <img
+              src={TestimonialAccent}
+              alt="Testimonial Frame"
+              className="position-absolute"
+              style={{ margin: `-30px 0 0 -30px`, zIndex: 1 }}
+            />
+          </div>
+        </div>
+
+        <div className="col-6">
+          <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+          <Star value={4.5} width={35} height={35} spacing={4}></Star>
+          <h5 className="h2 font-weight-light line-height-2 my-3">
+            {data.content}
+          </h5>
+          <span className="text-gray-500">
+            {data.familyName}, {data.familyOccupation}
+          </span>
+          <div>
+            <Button
+              className="btn px-5"
+              style={{ marginTop: 40 }}
+              hasShadow
+              isPrimary
+              type="link"
+              href={`/testimony/${data._id}`}
+            >
+              Read Their Story
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
