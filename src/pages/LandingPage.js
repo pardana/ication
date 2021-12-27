93db4e4fcce02526c4048f49c91801491bcef353
Aditria Pardana <aditria.pardana@yahoo.co.id>
2021-12-27 17:28:30 +0700

    styling component testimonial

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -15,7 +15,7 @@ export default class LandingPage extends Component {
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -14,18 +14,18 @@ export default function Testimony({ data }) {
               src={data.imageUrl}
               alt="Testimonial"
               className="position-absolute"
-              style={{ zIndex: 2 }}
+              style={{ zIndex: 1 }}
             />
             <img
               src={TestimonialAccent}
               alt="Testimonial Frame"
               className="position-absolute"
-              style={{ margin: `-30px 0 0 -30px`, zIndex: 1 }}
+              style={{ margin: `-30px 0 0 -30px` }}
             />
           </div>
         </div>
 
-        <div className="col-6">
+        <div className="col">
           <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
           <Star value={4.5} width={35} height={35} spacing={4}></Star>
           <h5 className="h2 font-weight-light line-height-2 my-3">
