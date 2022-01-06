fda0a30a7748f2a2a7cc7267995b59ff6aa05ac7
Aditria Pardana <aditria.pardana@yahoo.co.id>
2022-01-06 14:57:02 +0700

    Add testing inputDate and utils

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -4,16 +4,14 @@ import propTypes from "prop-types";
 import { DateRange } from "react-date-range";
 
 import "./index.scss";
-import "react-date-range/dist/style.css"; //main css file
-import "react-date-range/dist/theme/default.css"; //the css file
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 import formatDate from "utils/formatDate";
 import iconCalendar from "assets/images/icons/icon_calendar.svg";
 
-import React from "react";
-
 export default function Date(props) {
-  const [value, placeholder, name] = props;
+  const { value, placeholder, name } = props;
   const [isShowed, setIsShowed] = useState(false);
 
   const datePickerChange = (value) => {
