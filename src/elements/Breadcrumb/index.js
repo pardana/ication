42ec5ab40a090448a24dd6efee004675c2a0a6f8
Aditria Pardana <aditria.pardana@yahoo.co.id>
2022-01-07 14:20:06 +0700

    Add testing Breadcrumb

--- src/elements/Breadcrumb/index.js
+++ src/elements/Breadcrumb/index.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import propTypes from "prop-types";
 import Button from "elements/Button";
+
 import "./index.scss";
 
 export default function Breadcrumb(props) {
+  const className = ["breadcrumb", props.className];
   return (
     <nav aria-label="breadcrumb">
       <ol className={className.join(" ")}>
