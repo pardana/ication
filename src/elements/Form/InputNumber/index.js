57158595f08b55b247fe60e72e02e93707194427
Aditria Pardana <aditria.pardana@yahoo.co.id>
2022-01-12 21:17:43 +0700

    Update bookingForm.js

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -6,26 +6,27 @@ import "./index.scss";
 export default function Number(props) {
   const { value, placeholder, name, min, max, prefix, suffix, isSuffixPlural } =
     props;
-  const [InputValue, setInputValue] = useState(`${prefix}${suffix}`);
+  // const [InputValue, setInputValue] = useState(`${prefix}${suffix}`);
 
   const onChange = (e) => {
     let value = String(e.target.value);
-    if (prefix) value = value.replace(prefix);
-    if (suffix) value = value.replace(suffix);
+    // if (prefix) value = value.replace(prefix);
+    // if (suffix) value = value.replace(suffix);
 
-    const patternNumeric = new RegExp("[0-9]*");
-    const isNumeric = patternNumeric.test(value);
+    // const patternNumeric = new RegExp("[0-9]*");
+    // const isNumeric = patternNumeric.test(value);
 
-    if (isNumeric && +value <= max && +value >= min) {
+    // if (isNumeric && +value <= max && +value >= min) {
+    if (+value <= max && +value >= min) {
       props.onChange({
         target: {
           name: name,
           value: +value,
         },
       });
-      setInputValue(
-        `${prefix}${value}${suffix}${isSuffixPlural && value > 1 ? "s" : ""}`
-      );
+      // setInputValue(
+      // `${prefix}${value}${suffix}${isSuffixPlural && value > 1 ? "s" : ""}`
+      // );
     }
   };
 
@@ -61,10 +62,14 @@ export default function Number(props) {
           min={min}
           max={max}
           name={name}
-          pattern="[0-9]*"
+          readOnly
+          // pattern="[0-9]*"
           className="form-control"
           placeholder={placeholder ? placeholder : "0"}
-          value={String(InputValue)}
+          // value={String(InputValue)}
+          value={`${prefix}${value}${suffix}${
+            isSuffixPlural && value > 1 ? "s" : ""
+          }`}
           onChange={onChange}
         />
         <div className="input-grup-append">
--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import ItemDetails from "json/itemDetails.json";
 
@@ -23,21 +27,27 @@ export default class DetailsPage extends Component {
     return (
       <>
         <Header {...this.props} />
-        <PageDetailTitle
-          breadcrumb={breadcrumb}
-          data={ItemDetails}
-        ></PageDetailTitle>
-        <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+        <PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
+        <FeaturedImage data={ItemDetails.imageUrls} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr-5">
-              <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
             </div>
             <div className="col-5">
-              <BookingForm itemDetails={ItemDetails}></BookingForm>
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
             </div>
           </div>
         </section>
+
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+
+        <Footer />
       </>
     );
   }
